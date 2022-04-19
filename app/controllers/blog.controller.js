5f5151042cf27d0ccc5ce18e0118ca4017f17dc0
tanjimulislam55 <tanjimulislam118@gmail.com>
2022-04-19 10:06:44 +0600

    update controller erased

--- app/controllers/blog.controller.js
+++ app/controllers/blog.controller.js
@@ -95,29 +95,6 @@ exports.findOne = (req, res) => {
         });
   };
   
-// exports.update = (req, res) => {
-//     if (!req.body) {
-//         return res.status(400).send({
-//             message: "Data to update can not be empty!"
-//         });
-//     }
-
-//     const id = req.params.id;
-
-//     Blog.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-//     .then(data => {
-//         if (!data) {
-//         res.status(404).send({
-//             message: `Cannot update Blog with id=${id}. Maybe Blog was not found!`
-//         });
-//         } else res.send({ message: "Blog was updated successfully." });
-//     })
-//     .catch(err => {
-//         res.status(500).send({
-//         message: "Error updating Blog with id=" + id
-//         });
-//     });
-//   };
   
 exports.delete = async (req, res) => {
     const id = req.params.id;
